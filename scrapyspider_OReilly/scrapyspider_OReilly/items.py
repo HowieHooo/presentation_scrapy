a95da0b68569cbf390802328385007a762e03038
HowieHua <howiehua.hh@gmail.com>
2017-02-17 19:01:21 -0800

    adding the textbook example using xpath

---
+++ scrapyspider_OReilly/scrapyspider_OReilly/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class EmojiSpiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    emoji_handle = scrapy.Field()
+    emoji_image = scrapy.Field()
+    emoji_link = scrapy.Field()
+    section = scrapy.Field()
