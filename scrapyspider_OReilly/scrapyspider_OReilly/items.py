d4d9f9ecc3947719a9dd1ef9ebf9723be2abd8ee
HowieHua <howiehua.hh@gmail.com>
2017-02-24 11:54:13 -0800

    final

--- scrapyspider_OReilly/scrapyspider_OReilly/items.py
+++ scrapyspider_OReilly/scrapyspider_OReilly/items.py
@@ -18,14 +18,4 @@ class EmojiSpiderItem(scrapy.Item):
 
 
 
-class PythonPackageItem(scrapy.Item):
-	package_name = scrapy.Field()
-	version_number = scrapy.Field()
-	package_downloads = scrapy.Field()
-	package_page = scrapy.Field()
-	package_short_description = scrapy.Field()
-	home_page = scrapy.Field()
-	python_versions = scrapy.Field()
-	last_month_downloads = scrapy.Field()
-
 	
--- scrapyspider_OReilly/scrapyspider_OReilly/spiders/emo_spider.py
+++ scrapyspider_OReilly/scrapyspider_OReilly/spiders/emo_spider.py
@@ -9,20 +9,25 @@ class EmoSpider(scrapy.Spider):
 	]
 
 	def parse(self, response):
+                #find all h2 h3 selector in document
 		headers = response.xpath('//h2|//h3')
 		lists = response.xpath('//ul')
 		all_items = []
 		for header, list_cont in zip(headers, lists):
+			#get the emoji title
 			section = header.xpath('text()').extract()[0]
 			for li in list_cont.xpath('li'):
 				item = EmojiSpiderItem()
 				item['section'] = section
+				#get the two spans
 				spans = li.xpath('div/span')
 				if len(spans):
+					#first span contains link, extract it
 					link = spans[0].xpath('@data-src').extract()
 					if link:
 						item['emoji_link'] = response.url + link[0]
 					handle_code = spans[1].xpath('text()').extract()
+				#not a link, get the text
 				else:
 					handle_code = li.xpath('div/text()').extract()
 				if handle_code:
