72f8976121b6b095747b1df79a69ca10ee4376ec
HowieHua <howiehua.hh@gmail.com>
2017-02-27 17:39:49 -0800

    remove yield method, use item.py instead

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from tutorial.items import QuoteItem
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -9,9 +9,21 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        items = []
         for quote in response.css('div.quote'):
+            '''
             yield {
                 'text': quote.css('span.text::text').extract_first(),
                 'author': quote.css('small.author::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
+            '''
+            item = QuoteItem()
+            item['text'] = quote.css('span.text::text').extract_first()
+            item['author'] = quote.css('small.author::text').extract_first()
+            item['tags'] = quote.css('div.tags a.tag::text').extract_first()
+            items.append(item)
+
+        return items
+
+
